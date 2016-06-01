d3d35a2ccee0f11aa215e6fa1ad74ddf68cb6318
evefan <yixinfan2015@u.northwestern.edu>
2016-06-01 11:39:12 -0500

    back to top button!

--- script/script.js
+++ script/script.js
@@ -718,3 +718,26 @@ var chart = c3.generate({
   });
 
 }(jQuery, window, document));
+
+
+
+
+
+//back to top button!!!
+$(document).ready(function() {
+      // Show or hide the sticky footer button
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+          $('#top').fadeIn(200);
+        } else {
+          $('#top').fadeOut(200);
+        }
+      });
+      
+      // Animate the scroll to top
+      $('#top').click(function(event) {
+        event.preventDefault();
+        
+        $('html, body').animate({scrollTop: 0}, 300);
+      })
+    });
